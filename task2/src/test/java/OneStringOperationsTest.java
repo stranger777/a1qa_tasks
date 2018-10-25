a111cfc19bafdb065262c016136a8cda878f673b
Ivan Novikov <noviko-iva-money@yandex.ru>
2018-10-25 11:32:08 +0300

    initial commit

---
+++ task2/src/test/java/OneStringOperationsTest.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OneStringOperationsTest {
+
+    @Test
+    void getStr() {
+    }
+
+    @Test
+    void getAllWordsArray() {
+    }
+
+    @Test
+    void getAnyWord() {
+    }
+
+    @Test
+    void countPunctuationChars() {
+    }
+
+    @Test
+    void countWords() {
+    }
+}
---
+++ task2/src/test/java/ResourceExtractorTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ResourceExtractorTest {
+
+    @Test
+    void init() {
+    }
+
+    @Test
+    void init1() {
+    }
+}
